3ce2358a6addb65894179dd13085b88bf0d0df08
valentin <delo.biss13@gmail.com>
2021-08-27 16:54:16 +0300

    классы

---
+++ классы(es6)/script.js
@@ -0,0 +1,31 @@
+'use strict'
+
+class Rectangle {
+    constructor(height, widht) {
+        this.height = height;
+        this.widht = widht;
+    }
+
+    calcArea() {
+        return this.height * this.widht;
+    }
+}
+
+class ColoredRectangleWithText extends Rectangle {
+    constructor(height, widht, text, bgColor) {
+        super(height, widht);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+    showMyProps() {
+        console.log(`текст: ${this.text}, цвет: ${this.bgColor}`);
+    }
+}
+const div = new ColoredRectangleWithText(25, 10, 'hello world', 'red');
+div.showMyProps();
+console.log(div.calcArea());
+// const square = new Rectangle(10, 10);
+// const long = new Rectangle(20, 100);
+
+// console.log(square.calcArea());
+// console.log(long.calcArea());
