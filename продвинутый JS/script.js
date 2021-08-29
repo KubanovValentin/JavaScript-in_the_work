17c01c8c01a6eaa97f215b31715eddb6e2ad9f4a
valentin <delo.biss13@gmail.com>
2021-08-29 11:13:10 +0300

    JSON формат передачи данных,глубокое клонирование

--- продвинутый JS/script.js
+++ продвинутый JS/script.js
@@ -3,7 +3,21 @@
 // отправка
 const persone = {
     name: 'Alex',
-    tel: '+74444444'
+    tel: '+74444444',
+    parents: {
+        mom: 'Olga',
+        dad: 'Mike'
+    }
 };
+// глубокое клонирование обьекта
+const clone = JSON.parse(JSON.stringify(persone));
 
-console.log(JSON.stringify(persone));
+clone.parents.mom = 'Ann';
+console.log(persone);
+console.log(clone);
+// console.log(JSON.stringify(persone));
+
+// самый обычный обьект
+// console.log(JSON.parse(JSON.stringify(persone)));
+
+// преимущества маленький вес файла и простота понимания
